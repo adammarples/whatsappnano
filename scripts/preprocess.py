1308804f2039493f9e2e9f4a2408efbb9205d138
adam <adammarples@gmail.com>
2023-01-11 14:54:59 +0000

    more robust preprocessing

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -4,14 +4,15 @@ from pathlib import Path
 
 
 root = Path(__file__)
-data = root.parent.parent / "data/raw/bbb.txt"
+data = root.parent.parent / "data" / "raw" / "bbb.txt"
 date_length = len("06/01/2023, 15:23")
 prefix_length = len("06/01/2023, 15:23 -")
 dt_pattern = re.compile(r"\d{2}/\d{2}/\d{4}, \d{2}:\d{2}")
 
+assert data.exists()
 
 def main():
-    with data.open("r") as fi:
+    with data.open("r", encoding='utf-8') as fi:
         for line in fi:
             if not dt_pattern.match(line):
                 continue
@@ -19,10 +20,9 @@ def main():
             dt = datetime.strptime(date_str, "%d/%m/%Y, %H:%M")
             d = dt.date()
             outpath = root.parent.parent / f"data/preprocessed/{d:%Y%m%d}.txt"
-            text = line[prefix_length:].strip()
-            with outpath.open("a") as fo:
+            text = line[prefix_length:]
+            with outpath.open("a", encoding='utf-8') as fo:
                 fo.write(text)
-                fo.write("\n")
 
 
 if __name__ == "__main__":
